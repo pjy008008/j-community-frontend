3863cd0b70e10fdff72c034bb208e5713fb67607
Junyou Park <pjy008008@gmail.com>
2025-11-10 18:58:13 +0900

    feat(router): 로그인, 회원가입 페이지 라우팅 추가

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import PostDetailPage from "./pages/PostDetailPage";
 import CreatePostPage from "./pages/CreatePostPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Auth from "./pages/Auth";
 
 export const router = createBrowserRouter([
   {
@@ -26,9 +27,8 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    // 레이아웃이 필요 없는 별도 페이지 (예: 로그인, 회원가입)
-    // path: "/login",
-    // element: <LoginPage />,
+    path: "/auth",
+    element: <Auth />,
   },
   {
     path: "*",
